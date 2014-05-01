b367ddc8480e34e246e2312c4df5543030bd1d36
Jorge Lee <jrg.lee@gmail.com>
2014-04-30 23:27:54 -0700

    I have achieved clojure enlightenment. Namaste.
    Signed-off-by: Jorge Lee <jrg.lee@gmail.com>

--- src/koans/13_recursion.clj
+++ src/koans/13_recursion.clj
@@ -1,20 +1,27 @@
 (defn is-even? [n]
   (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+    true
+    (false? (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-      __
+      acc
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (if (= 0 (count coll))
+    coll
+    (cons (peek coll) (recursive-reverse (pop coll)))
+    ))
 
 (defn factorial [n]
-  __)
+  (loop [n   n
+         acc 1]
+    (if (= 1 n)
+      acc
+      (recur (dec n) (* n acc)))))
 
 (meditations
   "Recursion ends with a base case"
